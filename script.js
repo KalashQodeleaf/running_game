b9cfc7889555f00f295ae3cd776250e7a46d7c4a
KalashQodeleaf <kbharti@qodeleaf.com>
2024-07-01 16:37:00 +0530

    game

--- script.js
+++ script.js
@@ -1,51 +1,107 @@
 const obstacle = document.getElementById('obstacle');
+const obstacle2 = document.getElementById('obstacle2');
 const runner = document.getElementById('runner');
+const scoreBoard = document.getElementById('score');
 
 let animationDuration = 3000;
-const checkGameOver =() => {
-    const ob = obstacle.getBoundingClientRect();
-    const run = runner.getBoundingClientRect();
+let score = 0;
+let secondObject = false;
+let firstObject = false;
+let onJump = false;
+
+const getRandomSec = (min, max) => {
+    return (Math.random() * (max - min) + min).toFixed(0);
+}
+const animateObject = (element) => {
+    element.animate([
+        {
+            transform: 'translateX(-1500px)'
+        }
+    ],
+        {
+            duration: animationDuration
+        })
+}
+const doAnimation = (element, distence) => {
+    animateObject(element)
+    element.style.right = -distence + "px";
+}
+const gameInterval = setInterval(() => {
+    if (firstObject) {
+        doAnimation(obstacle, 500)
+        firstObject = false;
+    }
+    if (secondObject) {
+        doAnimation(obstacle2, 100)
+
+        secondObject = false;
+    }
+}, animationDuration)
+
+
+
+let distanceBetweenElement = (element1, element2) => {
+    const ob = element1.getBoundingClientRect();
+    const run = element2.getBoundingClientRect();
     const xDistance = run.x - ob.x;
     const yDistance = run.y - ob.y;
-    const distance = Math.sqrt(xDistance * xDistance + yDistance * yDistance);
-    if (distance < 80) {
-        document.getElementById("gameover").classList.remove('hidden')
-        const animation = obstacle.getAnimations()[0];
-        animation.pause()
-        clearInterval(checkGameInterval);
-        clearInterval(speedUp)
-        
+    let dis = Math.sqrt(xDistance * xDistance + yDistance * yDistance);
+
+    return dis;
+}
+const checkGameOver = () => {
+    scoreBoard.innerHTML = score;
+    score++;
+    if(score %100 === 0)
+    {
+        animationDuration -= 100;
     }
-  
+    
+    if (distanceBetweenElement(obstacle, runner) < 70 || distanceBetweenElement(obstacle2, runner) < 70) {
+        document.getElementById("gameover").classList.remove('hidden')
+        obstacle.style.animationPlayState = 'paused'
+        clearInterval(gameInterval);
+        clearInterval(checkGameInterval);
+        clearInterval(objectCome)
+
+    }
+
 }
 
-const jump = ()=>{
-    runner.style.transform="translateY(-150px)";
+const jump = () => {
+    
+    runner.style.transform = "translateY(-150px)";
     setTimeout(() => {
-        runner.style.transform="translateY(0px)";
+        runner.style.transform = "translateY(0px)";
+        
     }, 300);
+    setTimeout(() => {
+        onJump = false;
+    }, 600);
 }
-document.addEventListener('keydown',(e)=>{
-    if(e.key===" " || e.key==="w" || e.key==="W"){
+
+document.addEventListener('keydown', (e) => {
+    if(onJump)return;
+    if (e.key === " " || e.key === "w" || e.key === "W") {
+        
+        onJump =true;
         jump();
     }
 })
 
-
-// obstacle.addEventListener('animationiteration',()=>{
-//     // const random = Math.random()*3;
-//     animationDuration -=50;
-//     obstacle.style.animationDuration =  animationDuration +"ms";
-// })
-
-const speedUp = setInterval(()=>{
-    console.log("speed up");
-    animationDuration -=1;
-    obstacle.style.animationDuration= animationDuration+"ms";
-}
-,2000)
-const restart =()=>{
+const restart = () => {
     window.location.reload();
 }
-const checkGameInterval = setInterval(checkGameOver, 200);
-()=>speedUp;
+const objectCome = setInterval(() => {
+    if (!secondObject && getRandomSec(0, 100) < 40) {
+
+        secondObject = true;
+    }
+    if (!firstObject && getRandomSec(0, 100) >= 40) {
+
+        firstObject = true;
+    }
+}, animationDuration);
+const checkGameInterval = setInterval(checkGameOver, 50);
+() => gameInterval;
+() => objectCome;
